eb4b57bf5949fecfb4c54fb01474f2bdcbde001b
yuwande <33959191+yuwande@users.noreply.github.com>
2020-10-22 15:43:25 +0800

    Update login.py
    loginID/enpassword  fingerPrint

--- 咪咕视频登录_未知/login.py
+++ 咪咕视频登录_未知/login.py
@@ -3,6 +3,16 @@ Base_Url: https://www.migu.cn/
 Author: jing
 Modify: 2020/10/22
 """
+'''
+loginID/enpassword
+ var d = c.encrypt('');
+ b.siblings(".J_RsaAccout").val(d)
+
+ fingerPrint
+ q.page.rsaFingerprint(a.result.modulus, a.result.publicExponent).result
+ q.page.rsaFingerprint(a.result.modulus, a.result.publicExponent).details
+ "00833c4af965ff7a8409f8b5d5a83d87f2f19d7c1eb40dc59a98d2346cbb145046b2c6facc25b5cc363443f0f7ebd9524b7c1e1917bf7d849212339f6c1d3711b115ecb20f0c89fc2182a985ea28cbb4adf6a321ff7e715ba9b8d7261d1c140485df3b705247a70c28c9068caabbedbf9510dada6d13d99e57642b853a73406817"
+'''
 
 import requests
 import js2py
