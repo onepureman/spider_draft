1ef5a145491940762a1de2c9d04fa16941963d9e
unknown <jing@L3121100716171.addom.xinaogroup.com>
2020-10-26 17:38:29 +0800

    网站登陆破解

---
+++ 启信宝_未开始/login_demo.py
@@ -0,0 +1,46 @@
+"""
+Base_Url:
+Author:
+Modify:
+"""
+
+import execjs
+import requests
+from pprint import pprint
+
+
+class Login(object):
+
+    def __init__(self, user, pwd):
+        self.user = user
+        self.pwd = pwd
+        self.sess = requests.session()
+        self.login_url = ""
+
+    def get_pwd(self):
+
+        js_pwd = """
+        
+        function getpwd(pwd){
+        
+        
+        
+        }
+        
+        
+        """
+
+        pwd = execjs.compile(js_pwd).call("getpwd", self.pwd)
+        return pwd
+
+    def login_(self):
+        pwd = self.get_pwd()
+        print(pwd)
+
+
+if __name__ == '__main__':
+    user = ""
+    pwd = "222222"
+
+    login = Login(user, pwd)  # TODO: 输入账号&密码
+    login.login_()
